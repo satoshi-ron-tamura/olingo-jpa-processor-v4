547d5d448eff96f17d101b3c5ab93eff3d3bb399
satoshi-ron-tamura <ron84031@gmail.com>
2021-09-17 19:02:46 +0900

    add customizeSelection

--- jpa/odata-jpa-processor/src/main/java/com/sap/olingo/jpa/processor/core/api/JPAODataDatabaseProcessor.java
+++ jpa/odata-jpa-processor/src/main/java/com/sap/olingo/jpa/processor/core/api/JPAODataDatabaseProcessor.java
@@ -1,9 +1,13 @@
 package com.sap.olingo.jpa.processor.core.api;
 
-import java.util.List;
+import java.util.Collection;
 
-import javax.persistence.criteria.Selection;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.From;
 
+import com.sap.olingo.jpa.metadata.core.edm.mapper.api.JPAEntityType;
+import com.sap.olingo.jpa.metadata.core.edm.mapper.api.JPAPath;
 import com.sap.olingo.jpa.processor.core.database.JPAODataDatabaseSearch;
 import com.sap.olingo.jpa.processor.core.database.JPAODataDatabaseTableFunction;
 
@@ -13,7 +17,13 @@ import com.sap.olingo.jpa.processor.core.database.JPAODataDatabaseTableFunction;
  *
  */
 public interface JPAODataDatabaseProcessor extends JPAODataDatabaseSearch, JPAODataDatabaseTableFunction {
-  default List<Selection<?>> customizeSelection(List<Selection<?>> selections) {
-    return selections;
+  default Collection<JPAPath> customizeSelectionPath(
+      final CriteriaBuilder cb,
+      final CriteriaQuery<?> cq,
+      final From<?, ?> root,
+      final JPAEntityType entityType,
+      final Collection<JPAPath> selectionPath
+  ) {
+      return selectionPath;
   }
 }
